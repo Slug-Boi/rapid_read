5acec99c1de7129180335d8ad956707331b15b22
Theis <thph@itu.dk>
2024-06-15 01:24:29 +0200

    feat: pdf command almost complete

--- src/cmd/pdf.go
+++ src/cmd/pdf.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 
-	"github.com/sourcegraph/conc/panics"
 	"github.com/spf13/cobra"
 )
 
@@ -21,64 +19,30 @@ var pdfCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var PDFTool string
 
-		confDir, err := os.UserConfigDir()
-		if err != nil {
-			panic("User $HOME variable not set: " + err.Error())
+		// Get PDFTool from config file
+		config := Check_config()
+
+		PDFTool = config.PDFTool
+
+		if PDFTool == "" {
+			panic("PDFTool not set in config file")
 		}
-
-		_, err = os.Stat(confDir + "rapid_read/config.json")
-		if err != nil {
-			fmt.Println("Config file not found, would you like the program to create one for you? (y/n)")
-			var input string
-			fmt.Scanln(&input)
-
-			//TODO: Turn this into separate functions for readability and move it into root command file
-			if input == "y" {
-				err = os.Mkdir(confDir+"rapid_read", 0755)
-				if err != nil {
-					panic("Failed to create config directory: " + err.Error())
-				}
-
-				file , err := os.Create(confDir + "rapid_read/config.json")
-				if err != nil {
-					panic("Failed to create config file: " + err.Error())
-				}
-
-				_, err = file.WriteString("PDFTool: \"pdftotext\" \n")
-				if err != nil {
-					panic("Failed to write to config file: " + err.Error())
-				}
-
-				fmt.Println("Config file created, please edit the file to specify the PDF converting tool if you want to use a different tool than pdftotext.")
-
-				fmt.Println("Would you like to continue with the default PDF converting tool? (y/n)")
-				fmt.Scanln(&input)
-				if input == "y" {
-					PDFTool = "pdftotext"
-				} else {
-					fmt.Println("Exiting...")
-					os.Exit(1)
-				}
-			} else {
-				fmt.Println("Config file not found, exiting...")
-				os.Exit(1)
-			}
-		}
-
-
 
 		pdfCmd := exec.Command(PDFTool, args[0])
 
-		err = pdfCmd.Run()
+		err := pdfCmd.Run()
 		if err != nil {
 			panic("Failed to convert PDF file: " + err.Error())
 		}
 		fmt.Println(PDFTool + " called")
-
-
-		
+	
+		// Open the text file	
+		//TODO: Strip the file extension from the PDF file	
+		Open(args[0] + ".txt")
 	},
 }
+
+
 
 func init() {
 	rootCmd.AddCommand(pdfCmd)
