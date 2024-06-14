632d16487155b622224f60c7ebdba002d6e7f260
Theis <thph@itu.dk>
2024-06-15 01:24:15 +0200

    feat: Very basic open command

--- src/cmd/open.go
+++ src/cmd/open.go
@@ -21,20 +21,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("open called")
+		Check_config()
+
+		Open(args[0])
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(openCmd)
 
-	// Here you will define your flags and configuration settings.
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// openCmd.PersistentFlags().String("foo", "", "A help for foo")
+func Open(path string) {
+	fmt.Println("Opening file: ", path)
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// openCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Open the file in the GUI application
+
+	
+
 }
